d73417038d65c3285b5ecc7912cb1473bcb42a93
Panf <painfulluos@gmail.com>
2021-07-28 21:15:49 +0700

    init class Keyboard

---
+++ main.py
@@ -0,0 +1,10 @@
+import keyboard
+import pyperclip
+import time
+
+
+class Keyboard:
+	def __init__(self):
+			self.__rueng_lower = {'йцукенгшщзхъфывапролджэячсмитьбю.':"qwertyuiop[]asdfghjkl;'zxcvbnm,./"}
+			self.__rueng_upper = {'ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,':'QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?'}
+	
